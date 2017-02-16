b7dc355473c774fe14ba96b6f63ad4b8db895904
JoeMugwaneza <muganezajoseph@gmail.com>
2017-02-16 17:01:07 +0200

    made my app restful

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -18,8 +18,24 @@ class CompaniesController < ApplicationController
       :profession => params[:profession]}).body
 
     redirect_to "/companies/#{@company['id']}"
-
-
   end 
 
+  def edit
+    @company = Unirest.get("http://localhost:3000/api/v1/companies/#{params[:id]}.json").body
+  end 
+
+  def update
+    @company = Unirest.patch("http://localhost:3000/api/v1/companies/#{params[:id]}.json",
+      :headers => {"Accept" => "application/json"},
+      :parameters => {:name => params[:name],
+      :profession => params[:profession]}).body
+
+    redirect_to "/companies/#{@company["id"]}"
+  end 
+
+  def destroy
+
+    @company = Unirest.delete("http://localhost:3000/api/v1/companies/#{params[:id]}.json").body
+    redirect_to "/companies"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
   get "/companies/new", to: 'companies#new'
   get "/companies/:id", to: 'companies#show'
   post "/companies", to: 'companies#create'
+  get "companies/:id/edit", to: 'companies#edit'
+  patch "/companies/:id", to: 'companies#update'
+  delete "/companies/:id", to: 'companies#destroy'
 end
